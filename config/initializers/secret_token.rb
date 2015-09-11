399be450f09e722085130a99b72db94abe3308fb
Travis Dilweg <travis.dilweg@gmail.com>
2015-09-11 12:55:25 +0000

    First taste

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vkoos::Application.config.secret_key_base = 'b172176d7dcf5a902be0de7f7535596d3057bdd95c7fe3facad1c23efee4dc106e9e59e8649486c96f9e32e194e42d28d493868f1fbb150ecc342a93b5324920'
